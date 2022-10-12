5626109b6e015a6bfe5b80f74c67a859d969e0d2
jlbuild <juliabuildbot@gmail.com>
2022-10-12 09:42:13 +0000

    GStreamer_jll build 1.20.3+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -2,49 +2,21 @@
 export gst_inspect, gst_launch, gst_stats, gst_tester, gst_typefind, libgstbase, libgstcheck, libgstcontroller, libgstnet, libgstreamer
 
 using Glib_jll
-using LibUnwind_jll
-using Elfutils_jll
 using GMP_jll
 using GSL_jll
-using libcap_jll
 JLLWrappers.@generate_wrapper_header("GStreamer")
-JLLWrappers.@declare_executable_product(gst_inspect)
-JLLWrappers.@declare_executable_product(gst_launch)
-JLLWrappers.@declare_executable_product(gst_stats)
-JLLWrappers.@declare_executable_product(gst_tester)
-JLLWrappers.@declare_executable_product(gst_typefind)
 JLLWrappers.@declare_library_product(libgstbase, "libgstbase-1.0-0.dll")
 JLLWrappers.@declare_library_product(libgstcheck, "libgstcheck-1.0-0.dll")
 JLLWrappers.@declare_library_product(libgstcontroller, "libgstcontroller-1.0-0.dll")
 JLLWrappers.@declare_library_product(libgstnet, "libgstnet-1.0-0.dll")
 JLLWrappers.@declare_library_product(libgstreamer, "libgstreamer-1.0-0.dll")
+JLLWrappers.@declare_executable_product(gst_inspect)
+JLLWrappers.@declare_executable_product(gst_launch)
+JLLWrappers.@declare_executable_product(gst_stats)
+JLLWrappers.@declare_executable_product(gst_tester)
+JLLWrappers.@declare_executable_product(gst_typefind)
 function __init__()
-    JLLWrappers.@generate_init_header(Glib_jll, LibUnwind_jll, Elfutils_jll, GMP_jll, GSL_jll, libcap_jll)
-    JLLWrappers.@init_executable_product(
-        gst_inspect,
-        "bin\\gst-inspect-1.0.exe",
-    )
-
-    JLLWrappers.@init_executable_product(
-        gst_launch,
-        "bin\\gst-launch-1.0.exe",
-    )
-
-    JLLWrappers.@init_executable_product(
-        gst_stats,
-        "bin\\gst-stats-1.0.exe",
-    )
-
-    JLLWrappers.@init_executable_product(
-        gst_tester,
-        "bin\\gst-tester-1.0.exe",
-    )
-
-    JLLWrappers.@init_executable_product(
-        gst_typefind,
-        "bin\\gst-typefind-1.0.exe",
-    )
-
+    JLLWrappers.@generate_init_header(Glib_jll, GMP_jll, GSL_jll)
     JLLWrappers.@init_library_product(
         libgstbase,
         "bin\\libgstbase-1.0-0.dll",
@@ -73,6 +45,31 @@ function __init__()
         libgstreamer,
         "bin\\libgstreamer-1.0-0.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        gst_inspect,
+        "bin\\gst-inspect-1.0.exe",
+    )
+
+    JLLWrappers.@init_executable_product(
+        gst_launch,
+        "bin\\gst-launch-1.0.exe",
+    )
+
+    JLLWrappers.@init_executable_product(
+        gst_stats,
+        "bin\\gst-stats-1.0.exe",
+    )
+
+    JLLWrappers.@init_executable_product(
+        gst_tester,
+        "bin\\gst-tester-1.0.exe",
+    )
+
+    JLLWrappers.@init_executable_product(
+        gst_typefind,
+        "bin\\gst-typefind-1.0.exe",
     )
 
     JLLWrappers.@generate_init_footer()
